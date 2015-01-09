243337b555a26650ece401302d3c0d3f210a59b8
KinoLien <miecowbai@gmail.com>
2015-01-09 18:01:44 +0800

    Finally can work through the mobile and server.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,7 +3,8 @@ requirejs.config({
 		jquery: '/js/jquery.min',
 		three: '/js/three.min',
 		ioevents: '/js/events',
-		parallax: '/js/parallax.min'
+		parallax: '/js/parallax.min',
+		gamers: '/js/gamers'
 	},
 	shim: {
 		'parallax' : {
@@ -13,13 +14,21 @@ requirejs.config({
 });
 
 requirejs(['jquery',
-		   'ioevents', 
+		   'ioevents',
+		   'gamers', 
 		   'background', 
 		   'three',
 		   '/bower_components/threex/src/threex.minecraft/package.require.js',
-		   '/js/THREEx.KeyboardState.js'], function($, ioevents) {
+		   '/bower_components/threex.text/package.require.js',
+		   '/js/THREEx.KeyboardState.js'], function($, ioevents, Gamers) {
 
+		//ioevents.init();
+		var gamers = new Gamers();
 		
+		for(var i=0; i < 5; i++) {
+			gamers.push(i);
+		}
+
 		$(document.body).height($(window).height());
 		var $container = $('#canvas-wrap');
 
@@ -42,31 +51,38 @@ requirejs(['jquery',
 				NEAR,
 				FAR );
 
+
 		$container.append(renderer.domElement);
 
 		var updateFcts	= [];
 		var scene	= new THREE.Scene();
 		
 		camera.position.set(0, 1, 3);
-		camera.lookAt( new THREE.Vector3(0, 2, -10) );
+		//camera.lookAt( new THREE.Vector3(0, 2, -10) );
 
-		//////////////////////////////////////////////////////////////////////////////////
-		//		comment								//
-		//////////////////////////////////////////////////////////////////////////////////
 
-		var player	= new THREEx.MinecraftPlayer();
-		//player.character.root.position.y = -3;
-		
-		
-		scene.add(player.character.root);
+		littleMen = [];
 
-		updateFcts.push(function(delta, now){
-			player.update(delta, now)
+		function LittleMan() {
+			var player	= new THREEx.MinecraftPlayer();
+			scene.add(player.character.root);
+			updateFcts.push(function(delta, now){
+				player.update(delta, now);
+			});
+			player.character.root.position.x = (Math.random() * 1);
+			player.character.root.position.y = (Math.random() * 0.5);
+
+			player.character.loadWellKnownSkin('joker');
+
+			return player;
+		}
+
+		gamers.all().forEach( function(g) {
+			littleMen.push(LittleMan());
 		});
 		
-		// load a well known skin
-		player.character.loadWellKnownSkin('joker');
-				//////////////////////////////////////////////////////////////////////////////////
+		
+		//////////////////////////////////////////////////////////////////////////////////
 		//		controls.input based on keyboard				//
 		//////////////////////////////////////////////////////////////////////////////////
 
@@ -90,17 +106,49 @@ requirejs(['jquery',
 
 
 		document.body.addEventListener('keydown', function(event){
-			doKeyActions.call(player.controls.input, event.keyCode, event.shiftKey, true);
-		})
+			littleMen.forEach(function(lm) {
+				doKeyActions.call(lm.controls.input, event.keyCode, event.shiftKey, true);
+			});
+			console.log('key:'+event.keyCode+' press down');
+		});
+
 		document.body.addEventListener('keyup', function(event){
-			doKeyActions.call(player.controls.input, event.keyCode, event.shiftKey, false);
-		})
+			littleMen.forEach(function(lm) {
+				doKeyActions.call(lm.controls.input, event.keyCode, event.shiftKey, false);
+			});
+		});
 
 		ioevents.init((function(obj, callback){
 			return function(key, isKeydown){
-				callback.call(obj.controls.input, key, false, isKeydown);
+				obj.forEach(
+					(function(cb){
+						return function(lm){
+							cb.call(lm.controls.input, key, false, isKeydown);
+						};
+					})(callback)
+				);
 			};
-		})(player, doKeyActions));
+		})(littleMen, doKeyActions));
+
+		/////////////////////
+		// add text
+		/////////////////////
+		var canvas1 = document.createElement('canvas');
+			canvas1.width = 1000;
+			canvas1.height = 1000;
+			var context1 = canvas1.getContext('2d');
+			context1.font = "Bold 100px Helvetica";
+			context1.fillStyle = "rgba(255,0,0,0.95)";
+			context1.fillText('David', 0, 300);
+
+		  	// canvas contents will be used for a texture
+		  	var texture1 = new THREE.Texture(canvas1)
+		    texture1.needsUpdate = true;
+			var material = new THREE.SpriteMaterial( { map: texture1 } );	
+			spriteTL = new THREE.Sprite( material );
+			spriteTL.scale.set( 1, 1, 1 );
+			spriteTL.position.set(1,1,0);
+			scene.add(spriteTL);
 		//////////////////////////////////////////////////////////////////////////////////
 		//		render the scene						//
 		//////////////////////////////////////////////////////////////////////////////////
